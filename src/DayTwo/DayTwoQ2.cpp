289f756cfeb6d28d87a5938c48fe14a8c3a4a122
Alexis Rodriguez <alexisrod99@gmail.com>
2025-12-03 00:13:40 -0600

    finished day 2 part 2

--- src/DayTwo/DayTwoQ2.cpp
+++ src/DayTwo/DayTwoQ2.cpp
@@ -1,11 +1,60 @@
 #include "utils.hpp"
 #include <iostream>
 
-static constexpr const char *puzzle_input = "DayTwo/day_two_example.txt";
+auto check_repeating_substring(std::string_view str) -> bool;
+
+static constexpr const char *puzzle_input = "DayTwo/day_two.txt";
 
 int main(int argc, char **argv) {
   using namespace aoc::utils;
 
   auto contents = read(puzzle_input);
-  std::cout << contents << "\n";
+  auto ranges = split(contents, ",");
+
+  unsigned long long tally = 0;
+  for (const auto &range : ranges) {
+    auto values = split(range, "-");
+    auto start = std::stoll(values.front());
+    auto end = std::stoll(values.back());
+    for (auto i = start; i <= end; i++) {
+      auto val = std::to_string(i);
+      if (check_repeating_substring(val)) {
+        tally += i;
+        std::cout << i << " is invalid\n";
+        std::cout << "running tally=" << tally << "\n";
+      }
+    }
+  }
+  std::cout << "puzzle output: " << tally << std::endl;
+  // 65794984339
+  // +1 gold star
+}
+
+auto check_repeating_substring(std::string_view str) -> bool {
+  // current length of substring to check
+  std::cout << "inspecting " << str << "\n";
+  auto current_size = 1;
+  bool invalid = false;
+  while (current_size < str.size() && !invalid) {
+    // start at begining
+    for (auto i = 0; i < str.size(); i += current_size) {
+      auto to_check = str.substr(0, current_size);
+      bool matching = true;
+      for (auto j = current_size; j < str.size(); j += current_size) {
+        std::cout << "checking " << to_check << " against "
+                  << str.substr(j, current_size) << "\n";
+        if (to_check != str.substr(j, current_size)) {
+          matching = false;
+          break;
+        }
+      }
+      if (matching) {
+        invalid = true;
+        break;
+      }
+
+      current_size++;
+    }
+  }
+  return invalid;
 }
